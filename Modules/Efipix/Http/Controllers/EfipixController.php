507c8cffdcd4d42efa1fe30e7f358e14c34c4f13
Luiz Guilherme Chimenes <122306667+ChimenesLuiz@users.noreply.github.com>
2023-11-09 06:52:58 -0400

    A
    A

--- Modules/Efipix/Http/Controllers/EfipixController.php
+++ Modules/Efipix/Http/Controllers/EfipixController.php
@@ -20,71 +20,37 @@ class EfipixController extends Controller
     public function index()
     {
         $pixs = $this -> pix -> all();
-        return view('efipix::pix.index', [
-            'pixs' => $pixs
-        ]);
+        return view('efipix::pix.index') -> with('pixs', $pixs);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Renderable
-     */
     public function create(Request $request)
     {
         $flash = $request -> session() -> all();
-        return view('efipix::pix.create', ['flash' => $flash]);
+        return view('efipix::pix.create') -> with('flash', $flash);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Renderable
-     */
+
     public function store(StorePixRequest $request)
     {   
         $request -> validated();
 
-        $created = $this -> pix -> create($request -> except(['_token', 'btn_submit']));
-        if ($created)
-        {
-            
-            return redirect() -> route('efipix.index') -> with('message', 'Cadastrado com Sucesso!');
-        }
-        else
-        {
-            die('error');
-        }
+        $this -> pix -> create($request -> except(['_token', 'btn_submit'])); 
 
-        
+        return redirect() -> route('efipix.index') -> with('message', 'Cadastrado com Sucesso!');
     }
 
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
+
     public function show($id)
     {
         return view('efipix::show');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
     public function edit($id)
     {
         $data = $this -> pix -> findOrFail($id);
-        return view('efipix::pix.edit', ['data' => $data]);
+        return view('efipix::pix.edit') -> with('data', $data);
     }
 
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Renderable
-     */
     public function update(StorePixRequest $request, $id)
     {
         $request -> validated();
@@ -102,21 +68,9 @@ class EfipixController extends Controller
         return redirect() -> route('efipix.index') -> with('message', 'Editado com Sucesso!'); 
     }
 
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
-     */
     public function destroy($id)
     {
-        $created = $this -> pix -> destroy($id);
-        if ($created)
-        {
-            return redirect() -> route('efipix.index') -> with('message', 'Excluido com Sucesso!');
-        }
-        else
-        {
-            die('erro');
-        }
+        $this -> pix -> destroy($id);
+        return redirect() -> route('efipix.index') -> with('message', 'Excluido com Sucesso!');
     }
 }
